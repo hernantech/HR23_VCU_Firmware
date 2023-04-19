76cc5b3d98ffed5919785c9beafea6d0bfaecd22
hernantech <58359753+hernantech@users.noreply.github.com>
2023-04-19 14:54:10 -0700

    updated docs

--- docs/html/search/files_e.js
+++ docs/html/search/files_e.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['rms_5ffast_5fcharger_2ecpp_0',['rms_fast_charger.cpp',['../rms__fast__charger_8cpp.html',1,'']]],
-  ['rms_5ffast_5fcharger_2eh_1',['rms_fast_charger.h',['../rms__fast__charger_8h.html',1,'']]],
-  ['rms_5finverter_2ecpp_2',['rms_inverter.cpp',['../rms__inverter_8cpp.html',1,'']]],
-  ['rms_5finverter_2eh_3',['rms_inverter.h',['../rms__inverter_8h.html',1,'']]],
-  ['rom_2eh_4',['rom.h',['../rom_8h.html',1,'']]],
-  ['rom_5fmap_2eh_5',['rom_map.h',['../rom__map_8h.html',1,'']]],
-  ['rtos_5fbindings_2eh_6',['rtos_bindings.h',['../rtos__bindings_8h.html',1,'']]]
+  ['readme_2emd_0',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
+  ['rms_5ffast_5fcharger_2ecpp_1',['rms_fast_charger.cpp',['../rms__fast__charger_8cpp.html',1,'']]],
+  ['rms_5ffast_5fcharger_2eh_2',['rms_fast_charger.h',['../rms__fast__charger_8h.html',1,'']]],
+  ['rms_5finverter_2ecpp_3',['rms_inverter.cpp',['../rms__inverter_8cpp.html',1,'']]],
+  ['rms_5finverter_2eh_4',['rms_inverter.h',['../rms__inverter_8h.html',1,'']]],
+  ['rom_2eh_5',['rom.h',['../rom_8h.html',1,'']]],
+  ['rom_5fmap_2eh_6',['rom_map.h',['../rom__map_8h.html',1,'']]],
+  ['rtos_5fbindings_2eh_7',['rtos_bindings.h',['../rtos__bindings_8h.html',1,'']]]
 ];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hr23_5fvcu_5ffirmware_0',['HR23_VCU_Firmware',['../md__r_e_a_d_m_e.html',1,'']]]
+];
